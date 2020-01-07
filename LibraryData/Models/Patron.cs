7d6e5d182d03341babc72f60698bc7c0595fddb2
Shallu Nah <renovatio3d@gmail.com>
2020-01-06 21:56:55 -0700

    Initial commit

---
+++ LibraryData/Models/Patron.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData.Models
+{
+    public class Patron
+    {
+        public int Id { get; set; }
+        public int FirstName { get; set; }
+        public int LastName { get; set; } 
+        public int Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string TelephonNumber { get; set; }
+        
+        //public virtual LibraryCard LibraryCard { get; set; }
+    }
+}
